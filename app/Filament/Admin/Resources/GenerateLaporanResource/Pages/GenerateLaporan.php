912a5e64bf95c51ee2b30d867af4364d62abe130
druzxh <badrudin.on@gmail.com>
2025-10-10 23:27:40 +0700

    fix: compose & generate laporann

---
+++ app/Filament/Admin/Resources/GenerateLaporanResource/Pages/GenerateLaporan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Admin\Resources\GenerateLaporanResource\Pages;
+
+use App\Filament\Admin\Resources\GenerateLaporanResource;
+use Filament\Resources\Pages\Page;
+
+class GenerateLaporan extends Page
+{
+    protected static string $resource = GenerateLaporanResource::class;
+
+    protected static string $view = 'filament.admin.resources.generate-laporan-resource.pages.generate-laporan';
+    
+}
